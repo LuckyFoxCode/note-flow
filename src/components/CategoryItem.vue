065cbbb2b4d2d7316c87367d0eef77580eca6621
LuckyFoxCode <sergei181989@gmail.com>
2026-04-24 22:11:58 +0300

    refactor: extract CategoryItem component

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { IconCircle } from '@/assets/icons';
+import type { Category } from '@/types';
+
+const { category, percentageCount } = defineProps<{
+  category: Category;
+  percentageCount: number;
+}>();
+
+const formatDate = (date: Date) => {
+  const today = new Date();
+
+  const isToday =
+    date.getDay() === today.getDay() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear();
+
+  if (isToday) return 'Today';
+
+  return new Intl.DateTimeFormat('en-US').format(date);
+};
+</script>
+<template>
+  <li>
+    <router-link
+      :to="`/categories/${category.slug}`"
+      class="focus-within:border-accent border-border shadow-border category-link flex w-full flex-col rounded-lg border-2 px-2 py-2 shadow transition-all duration-200 outline-none hover:-translate-y-1 hover:shadow-lg md:p-1"
+      :style="{ '--category-color': category.categoryColor }"
+    >
+      <div class="mb-10 flex w-full items-center justify-between">
+        <h3 class="mr-5 text-sm md:text-base 2xl:text-lg">{{ category.name }}</h3>
+        <IconCircle
+          :percentage="percentageCount"
+          :color="category.categoryColor"
+        />
+      </div>
+      <div class="text-text-secondary flex w-full justify-end text-xs md:text-sm 2xl:text-base">
+        <span
+          class="rounded-lg border-t px-2 py-1"
+          :style="{
+            borderColor: category.categoryColor,
+            boxShadow: `-2px -2px 8px -3px ${category.categoryColor}`,
+          }"
+          >{{ formatDate(category.createdAt) }}</span
+        >
+      </div>
+    </router-link>
+  </li>
+</template>
--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
 import type { State } from '@/App.vue';
-import { IconCircle } from '@/assets/icons';
+import CategoryItem from '@/components/CategoryItem.vue';
 import TheHeader from '@/components/TheHeader.vue';
 import { inject, ref } from 'vue';
 
 const openOverlay = inject<(type: 'category' | 'note') => void>('openOverlay');
 const state = inject<State>('state');
-
-const formatDate = (date: Date) => {
-  const today = new Date();
-
-  const isToday =
-    date.getDay() === today.getDay() &&
-    date.getMonth() === today.getMonth() &&
-    date.getFullYear() === today.getFullYear();
-
-  if (isToday) return 'Today';
-
-  return new Intl.DateTimeFormat('en-US').format(date);
-};
 
 const handleOpenOverlay = () => {
   if (openOverlay) {
@@ -35,7 +22,7 @@ const onClickCount = () => {
 <template>
   <section class="flex h-screen flex-col gap-1">
     <TheHeader>
-      <span>Notes Page</span>
+      <span>Categories Page</span>
     </TheHeader>
     <div>
       <button
@@ -55,34 +42,12 @@ const onClickCount = () => {
       v-if="state?.categories.length"
       class="grid min-h-0 flex-1 grid-cols-1 content-start gap-1 overflow-y-auto p-1 md:grid-cols-2 md:gap-2 md:px-2 lg:grid-cols-3 lg:gap-3 xl:grid-cols-4 2xl:gap-4"
     >
-      <li
-        v-for="category in state?.categories"
+      <CategoryItem
+        v-for="category in state.categories"
         :key="category.id"
-      >
-        <router-link
-          :to="`/categories/${category.slug}`"
-          class="focus-within:border-accent border-border shadow-border category-link flex w-full flex-col rounded-lg border-2 px-2 py-2 shadow transition-all duration-200 outline-none hover:-translate-y-1 hover:shadow-lg md:p-1"
-          :style="{ '--category-color': category.categoryColor }"
-        >
-          <div class="mb-10 flex w-full items-center justify-between">
-            <h3 class="mr-5 text-sm md:text-base 2xl:text-lg">{{ category.name }}</h3>
-            <IconCircle
-              :percentage="percentageCount"
-              :color="category.categoryColor"
-            />
-          </div>
-          <div class="text-text-secondary flex w-full justify-end text-xs md:text-sm 2xl:text-base">
-            <span
-              class="rounded-lg border-t px-2 py-1"
-              :style="{
-                borderColor: category.categoryColor,
-                boxShadow: `-2px -2px 8px -3px ${category.categoryColor}`,
-              }"
-              >{{ formatDate(category.createdAt) }}</span
-            >
-          </div>
-        </router-link>
-      </li>
+        :category="category"
+        :percentage-count="percentageCount"
+      />
     </ul>
     <span v-else>Empty categories</span>
   </section>
